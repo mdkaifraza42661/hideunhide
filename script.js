bc5cc36314074ab712eadf3706080b86e552b8e8
unknown <kaifraza42661@gmail.com>
2022-05-05 12:27:16 +0530

    Second

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $(".fix_a").mouseenter(function() {
+        $("#ksFooterHoverSection01").css("display", "block")
+    })
+    $(".fix_a").mouseleave(function() {
+        $("#ksFooterHoverSection01").css("display", "none")
+    })
+    $(".fix_b").mouseenter(function() {
+        $("#ksFooterHoverSection02").css("display", "block")
+    })
+    $(".fix_b").mouseleave(function() {
+        $("#ksFooterHoverSection02").css("display", "none")
+    })
+
+
+
+})
